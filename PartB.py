249c251a5432e520ae24db2cef9b1d5ce42c2445
ibrah113 <146074640+ibrah113@users.noreply.github.com>
2025-03-24 14:37:58 +0000

    cooked

--- PartB.py
+++ PartB.py
@@ -11,3 +11,13 @@ class TestStaffTeacher(unittest.TestCase):
     def test_is_instance_of_teacher(self):
         teacher1 = Teacher("Ibrahim Shaban", "11/03/2004", "Male", 1002, "Tud Blanch", "Mathematics", 8)
         self.assertIsInstance(teacher1, Teacher)  
+
+    def test_is_not_instance_of_teacher(self):
+        staff1 = Staff("Mike Kazakovas", "15/04/2004", "Male", 1001, "Tud Blanch")
+        self.assertNotIsInstance(staff1, Teacher)  
+
+    def test_is_not_instance_of_staff(self):
+        teacher1 = Teacher("Ibrahim Shaban", "11/03/2004", "Male", 1002, "Tud Blanch", "Mathematics", 8)
+        self.assertNotIsInstance(teacher1, Staff)  
+
+
